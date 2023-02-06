f76a94cc66e6da45c3d5eb0e0efee4d6626d3163
NickVerastegui <NickVerastegui@gmail.com>
2023-02-06 12:21:13 -0500

    Added weight struct

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,8 +1,20 @@
-pub struct Weights();
+pub struct Weights {
+    pub bus_factor: f64,
+    pub correctness_factor: f64,
+    pub ramp_up_time: f64,
+    pub responsiveness: f64,
+    pub license_compatibility: f64,
+}
 
 impl Weights {
     pub fn new() -> Self {
-        Weights {}
+        Weights {
+            bus_factor: 0.,
+            correctness_factor: 0.,
+            ramp_up_time: 0.,
+            responsiveness: 0.,
+            license_compatibility: 0.,
+        }
     }
 }
 
