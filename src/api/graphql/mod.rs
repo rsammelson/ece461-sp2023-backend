5ce463e22e746a014c53a15d40a8a151d89f3a9b
Robert Sammelson <robert@sammelson.com>
2023-02-12 23:54:59 -0500

    Add responsiveness metric

--- src/api/graphql/mod.rs
+++ src/api/graphql/mod.rs
@@ -1,24 +1,54 @@
+use super::fetch::GithubRepositoryName;
 use crate::{
     api::{self, APIError},
     log::{self, LogLevel},
 };
-use graphql_client::{reqwest::post_graphql, GraphQLQuery};
-use std::error::Error;
 
-use super::fetch::GithubRepositoryName;
+use self::responsiveness_query::{
+    CommentAuthorAssociation, ResponsivenessQueryRepositoryIssuesNodes,
+};
+
+use graphql_client::{reqwest::post_graphql, GraphQLQuery};
+use serde::Deserialize;
+use std::error::Error;
 
 // #[allow(clippy::upper_case_acronyms)]
 // type URI = String;
-// type DateTime = String;
+
+#[derive(Debug)]
+pub struct DateTime(chrono::DateTime<chrono::Utc>);
+
+impl<'de> Deserialize<'de> for DateTime {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s: &str = serde::Deserialize::deserialize(deserializer)?;
+        let dt = chrono::DateTime::parse_from_rfc3339(s).map_err(serde::de::Error::custom)?;
+        Ok(DateTime(dt.into()))
+    }
+}
 
 #[allow(dead_code)]
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/api/graphql/schema.json",
-    query_path = "src/api/graphql/license.graphql",
+    query_path = "src/api/graphql/queries.graphql",
     response_derives = "Debug"
 )]
 struct LicenseQuery {
+    owner: String,
+    name: String,
+}
+
+#[allow(dead_code)]
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/api/graphql/schema.json",
+    query_path = "src/api/graphql/queries.graphql",
+    response_derives = "Debug"
+)]
+struct ResponsivenessQuery {
     owner: String,
     name: String,
 }
@@ -27,6 +57,12 @@ struct LicenseQuery {
 #[async_trait::async_trait]
 pub trait Queryable {
     async fn query_license(&self) -> Result<Option<String>, Box<dyn Error + Send + Sync>>;
+    async fn query_responsiveness(
+        &self,
+    ) -> Result<
+        Vec<Result<chrono::Duration, Box<dyn Error + Send + Sync>>>,
+        Box<dyn Error + Send + Sync>,
+    >;
 }
 
 #[async_trait::async_trait]
@@ -67,5 +103,89 @@ impl Queryable for GithubRepositoryName {
         }
 
         Ok(license_info.spdx_id)
+    }
+
+    async fn query_responsiveness(
+        &self,
+    ) -> Result<
+        Vec<Result<chrono::Duration, Box<dyn Error + Send + Sync>>>,
+        Box<dyn Error + Send + Sync>,
+    > {
+        let client = api::get_github_client()?;
+
+        let vars = responsiveness_query::Variables {
+            owner: self.owner.to_owned(),
+            name: self.name.to_owned(),
+        };
+
+        log::log(
+            LogLevel::None,
+            &format!(
+                "Starting responsiveness query for {}/{}",
+                &self.owner, &self.name
+            ),
+        );
+
+        let response_body =
+            post_graphql::<ResponsivenessQuery, _>(&client, "https://api.github.com/graphql", vars)
+                .await?;
+
+        let response_data = response_body
+            .data
+            .ok_or(APIError::NoReponseError("querying license"))?;
+
+        let issues = response_data
+            .repository
+            .and_then(|r| r.issues.nodes)
+            .ok_or(APIError::InvalidResponseError(
+                "missing issue nodes while querying responsiveness",
+            ))?;
+
+        let times = issues.iter().flatten().map(issue_to_response_time);
+        Ok(times.collect())
+    }
+}
+
+fn issue_to_response_time(
+    issue: &ResponsivenessQueryRepositoryIssuesNodes,
+) -> Result<chrono::Duration, Box<dyn Error + Send + Sync>> {
+    let start = &issue.created_at;
+    let comments = issue
+        .comments
+        .nodes
+        .as_ref()
+        .ok_or(APIError::InvalidResponseError(
+            "issue to response time issue didn't have commend nodes",
+        ))?;
+    let end = comments
+        .iter()
+        .flatten()
+        .find(|c| check_author_association(&c.author_association))
+        .map(|c| &c.created_at);
+
+    let now = DateTime(chrono::Utc::now());
+    let mut end = match end {
+        Some(t) => t,
+        None => &now,
+    };
+
+    if issue.closed {
+        if let Some(close_time) = &issue.closed_at {
+            if close_time.0 < end.0 {
+                end = &close_time;
+            }
+        }
+    }
+
+    Ok(end.0 - start.0)
+}
+
+fn check_author_association(association: &CommentAuthorAssociation) -> bool {
+    match association {
+        CommentAuthorAssociation::MEMBER => true,
+        CommentAuthorAssociation::OWNER => true,
+        CommentAuthorAssociation::COLLABORATOR => true,
+        CommentAuthorAssociation::CONTRIBUTOR => true,
+        _ => false,
     }
 }
--- src/controller/responsiveness.rs
+++ src/controller/responsiveness.rs
@@ -1,4 +1,4 @@
-use crate::controller::*;
+use crate::{api::graphql::Queryable, controller::*};
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub struct Responsiveness();
@@ -8,13 +8,38 @@ impl Scorer for Responsiveness {
     async fn score(
         &self,
         _repo: &Mutex<git2::Repository>,
-        url: &GithubRepositoryName,
+        repo_identifier: &GithubRepositoryName,
     ) -> Result<(Metric, f64), Box<dyn Error + Send + Sync>> {
         log::log(
             LogLevel::All,
-            &format!("Starting to analyze Responsiveness for {url}"),
+            &format!("Starting to analyze Responsiveness for {repo_identifier}"),
         );
 
-        Ok((Metric::Responsiveness(Responsiveness()), -1.))
+        let response_times = repo_identifier.query_responsiveness().await?;
+        let response_times = response_times.iter().map(|r| r.as_ref().ok()).flatten();
+
+        let (n, total_time) = response_times.fold((0, chrono::Duration::zero()), |(n, sum), t| {
+            (n + 1, sum + *t)
+        });
+
+        if n <= 0 {
+            log::log(
+                LogLevel::All,
+                &format!("Got response time 0 for {repo_identifier}, due to no responses"),
+            );
+
+            Ok((Metric::Responsiveness(Responsiveness()), 0.))
+        } else {
+            let average_time = (total_time / n).num_seconds() as f64;
+            let x: f64 = average_time / (60. * 60. * 24. * 3.) + 1.;
+            let score = 1. / x;
+
+            log::log(
+                LogLevel::All,
+                &format!("Got responsiveness {score} for {repo_identifier}"),
+            );
+
+            Ok((Metric::Responsiveness(Responsiveness()), score))
+        }
     }
 }
