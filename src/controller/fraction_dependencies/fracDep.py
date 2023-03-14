d201978743fdc9e92c6c0789545190c7e09c661e
NickW09 <60638129+NickW09@users.noreply.github.com>
2023-03-13 23:59:03 -0500

    Updates

--- src/controller/fraction_dependencies/fracDep.py
+++ src/controller/fraction_dependencies/fracDep.py
@@ -1,4 +1,30 @@
 #Locate cloned repo
+##API->Fetch->mod.rs
+##$HOME/.cache/acme/{repo_owner}/{repo_name}
+
 #Locate package.json
+
 #Open package.json
+
 #Count dependencies
+
+#NEED TO ADD DEPENDENCY?
+import os
+import json
+
+file = '$HOME/.cache/acme'
+
+def findDeps(repo_owner, repo_name):
+    #check slashes
+    file = file + '/' + repo_owner + '/' + repo_name + '/' + 'package.json'
+    exist = os.path.isfile(file)
+
+    if exist:
+        with open('file') as json_file:
+            data = json.load(json_file)
+        count = len(data["devDependencies"])
+        return count
+
+    else:
+        print("error, no path")
+        return -1
--- src/controller/fraction_dependencies/mod.rs
+++ src/controller/fraction_dependencies/mod.rs
@@ -8,5 +8,16 @@ pub struct Correctness();
 
 #[async_trait]
 impl Scorer for FractionDependencies {
-    
+    //grab repo owner (arg1)
+    //grab repo name (arg2)
+    //pass to python code
+    use std::process::Command;
+    let output = Command::new("python3")
+                        .arg("fracDep.py")
+                        .arg("input1")
+                        .arg("input2")
+                        .output()
+                        .expect("file.py broke :(");
+    //use output num of dependencies to calc score
+    //save or return score 
 }
