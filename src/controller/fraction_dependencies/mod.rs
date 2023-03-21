c86bad149e7ba61f9e3649a2722e79d96e2cabf0
reed369 <reed369@purdue.edu>
2023-03-20 20:41:09 -0400

    fraction_dependencies error debugging

--- src/controller/fraction_dependencies/mod.rs
+++ src/controller/fraction_dependencies/mod.rs
@@ -2,24 +2,56 @@
 mod tests;
 
 use crate::controller::*;
+use std::process::Command;
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
-pub struct Correctness();
+pub struct FractionDependencies();
 
 #[async_trait]
 impl Scorer for FractionDependencies {
     //grab repo owner (arg1)
     //grab repo name (arg2)
     //pass to python code
-    use std::process::Command;
-    let output = Command::new("python3")
-                        .arg("fracDep.py")
-                        .arg("input1")
-                        .arg("input2")
-                        .output()
-                        .expect("file.py broke :(");
+    async fn score<Q>(
+        &self,
+        repo: &Mutex<git2::Repository>,
+        repo_identifier: &Q,
+    ) -> Result<(Metric, f64), Box<dyn Error + Send + Sync>>
+    where
+        Q: Queryable + fmt::Display + Sync + 'static,
+    {
+        log::log(
+            LogLevel::All,
+            &format!("Starting to analyze FractionDependencies for {repo_identifier}"),
+        );
+        
+        let repo = repo.lock().await;
+
+        // let output = -1.0;
+        print!("{}\n", repo_identifier);
+
+        let out = 1.0;
+
+        // let out = Command::new("python3")
+        //                 .arg("fracDep.py")
+        //                 .arg("nothing")
+        //                 .output()
+        //                 .expect("file.py broke :(");
+        print!("{:?}\n", out);
     //use output num of dependencies to calc score
     //save or return score 
 
-    Ok((Metric::FractionDependencies(FractionDependencies()), (1/output.output)))
+        log::log(
+            LogLevel::All,
+            &format!("Done analyzing FractionDependencies for {repo_identifier}"),
+        );
+
+
+        Ok((
+            Metric::FractionDependencies(FractionDependencies()), 
+            (-1.0),
+            // (1.0/out),
+        ))
+        
+    }
 }
--- src/controller/fraction_reviewed/mod.rs
+++ src/controller/fraction_reviewed/mod.rs
@@ -4,9 +4,23 @@ mod tests;
 use crate::controller::*;
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
-pub struct Correctness();
+pub struct FractionReviewed();
 
 #[async_trait]
-impl Scorer for Reviewed {
-    Ok((Metric::FractionReviewed(FractionReviewed()), -1.))
+impl Scorer for FractionReviewed {
+    async fn score<Q>(
+        &self,
+        _repo: &Mutex<git2::Repository>,
+        repo_identifier: &Q,
+    ) -> Result<(Metric, f64), Box<dyn Error + Send + Sync>>
+    where
+        Q: Queryable + fmt::Display + Sync + 'static,
+    {
+        log::log(
+            LogLevel::All,
+            &format!("Starting to analyze FractionReviewed for {repo_identifier}"),
+        );
+
+        Ok((Metric::FractionReviewed(FractionReviewed()), -1.))
+    }
 }
