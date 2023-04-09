3d7c807005b78fb8fafa111997810b11f6319e2e
NickW09 <nickcreo@yahoo.com>
2023-04-09 14:45:01 -0400

    Update mod.rs

--- src/controller/fraction_reviewed/mod.rs
+++ src/controller/fraction_reviewed/mod.rs
@@ -21,6 +21,6 @@ impl Scorer for FractionReviewed {
             &format!("Starting to analyze FractionReviewed for {repo_identifier}"),
         );
 
-        Ok((Metric::FractionReviewed(FractionReviewed()), -1.));
+        Ok((Metric::FractionReviewed(FractionReviewed()), -1.))
     }
 }
